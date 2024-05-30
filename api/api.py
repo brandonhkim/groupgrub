bc4cf9a3121805c182c49e9accd2a9ed7fd853f2
hyuk <hyuk@hyuks-MBP.lan>
2024-05-30 14:09:14 -0700

    added rough draft of overall routing

--- api/api.py
+++ api/api.py
@@ -4,9 +4,41 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# TODO: replace with home page
 @app.route('/time')
 def get_current_time():
-    fr = FusionRepository()
+    fusion_repo = FusionRepository()
+    # print(fusion_repo.getRestaurants(['33.866669', '-117.566666'], ['mcdonalds'], 3))
     return {'time': time.time()}
+
+# NOTE: research data persistence, keep logged in
+@app.route('/login')
+def login():
+    raise NotImplementedError
+
+
+@app.route('/register')
+def register():
+    raise NotImplementedError
+
+
+@app.route('/lobby')
+def host_join():
+    # TODO: Part 1 - host or join
+    # TODO: Part 2 - lobby
+    raise NotImplementedError
+
+
+# joining actual lobby
+@app.route('/lobby<code>')
+def lobby():
+    raise NotImplementedError
+
+
+@app.route('/lobby<code>/game')
+def game():
+    raise NotImplementedError
+
+
 
     
